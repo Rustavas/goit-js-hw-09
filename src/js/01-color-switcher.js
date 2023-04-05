263921bb93537e33170412abc15a9e3623ac161e
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-04-05 12:25:08 +0300

    task-01(fault)

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,16 +6,17 @@ const refs = {
 const randomColor = function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
-let timerId = setInterval(randomColor(), 1000);
-console.log(timerId);
+document.body.style.backgroundColor = setInterval(randomColor, 1000);
 
-// refs.startBtn.addEventListener('click', changeColor)
-refs.stopBtn.addEventListener('click', clearInterval(timerId))
+const timerId = setInterval(randomColor, 1000);
 
+refs.startBtn.addEventListener('click', changeColor);
+refs.stopBtn.addEventListener('click', stopChangeColor);
 
-function changeColor() {
-  document.body.style.backgroundColor = setInterval(randomColor(), 1000);
+function changeColor () {
+  document.body.style.backgroundColor = setInterval(randomColor, 1000);
 }
-// function stopChangeColor () {
-//   clearInterval(timerId);
-// }
+
+function stopChangeColor () {
+  clearInterval(timerId);
+}
