ccd01b2a50593dbdedcf0513d543997c6aba5736
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-04-05 23:06:20 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -6,7 +6,6 @@ const refs = {
  function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
 }
-// document.body.style.backgroundColor = setInterval(randomColor, 1000);
 
 let timerId = null;
 
