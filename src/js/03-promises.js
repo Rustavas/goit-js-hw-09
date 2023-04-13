7e53d703a7de6a2dba53d6669f3d9d1559b45c7b
RuslanIvasiunko <115837907+Rustavas@users.noreply.github.com>
2023-04-13 12:25:11 +0300

    Update 03-promises.js

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -13,19 +13,20 @@ function startGeneration (event){
     let firstDelay = event.currentTarget.elements.delay.value;
     let stepDelay = event.currentTarget.elements.step.value;
     let amountEl = event.currentTarget.elements.amount.value;
-  for(let i=1; i <= amountEl; i += 1 ){
+
+  for(let i = 1; i <= amountEl; i += 1 ){
+
     createPromise(i, firstDelay)
   .then(({ position, delay }) => {
-    // console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
     Notify.success('✅ Fulfilled promise ${position} in ${delay}ms')
   })
   .catch(({ position, delay }) => {
-    // console.log(`❌ Rejected promise ${position} in ${delay}ms`);
     Notify.failure('❌ Rejected promise ${position} in ${delay}ms');
   });
     firstDelay += stepDelay;
-  }
-}
+  };
+};
+
 function createPromise(position, delay) { 
   return new Promise((resolve, reject) => {
     const shouldResolve = Math.random() > 0.3;
